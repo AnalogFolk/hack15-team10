c7e31204058a4a0a14cb5a400ddc3debe07a24e2
Jolyon Russ <jolyon.russ@telegraph.co.uk>
2015-07-04 18:23:46 +0100

    Removed mongo, mongoose and pg

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 var express        = require('express'),
     path           = require('path'),
-    mongoose       = require('mongoose'),
+    // mongoose       = require('mongoose'),
     hbs            = require('express-hbs'),
     logger         = require('morgan'),
     bodyParser     = require('body-parser'),
@@ -16,10 +16,10 @@ var express        = require('express'),
     routes         = require('./routes');
 
 
-mongoose.connect(config.database.url);
-mongoose.connection.on('error', function () {
-  console.log('mongodb connection error');
-});
+// mongoose.connect(config.database.url);
+// mongoose.connection.on('error', function () {
+//   console.log('mongodb connection error');
+// });
 
 var app = express();
 
